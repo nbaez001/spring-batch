b21f0888372790e3c86973051d8bfb436d89be4a
nerio.baez <nbaez001@gmail.com>
2025-09-06 01:15:46 -0500

    Flat file item reader

--- src/main/java/com/empresa/proyecto/config/SampleJob.java
+++ src/main/java/com/empresa/proyecto/config/SampleJob.java
@@ -1,97 +1,77 @@
 package com.empresa.proyecto.config;
 
-import com.empresa.proyecto.listener.FirstJobListener;
-import com.empresa.proyecto.listener.FirstStepListener;
-import com.empresa.proyecto.processor.FirstItemProcessor;
-import com.empresa.proyecto.reader.FirstItemReader;
-import com.empresa.proyecto.service.SecondTasklet;
+import com.empresa.proyecto.model.StudentCsv;
 import com.empresa.proyecto.writer.FirstItemWriter;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class SampleJob {
 
     @Autowired
-    private SecondTasklet secondTasklet;
-
-    @Autowired
-    private FirstJobListener firstJobListener;
-
-    @Autowired
-    private FirstStepListener firstStepListener;
-
-    @Autowired
-    private FirstItemReader firstItemReader;
-
-    @Autowired
-    private FirstItemProcessor firstItemProcessor;
-
-    @Autowired
     private FirstItemWriter firstItemWriter;
 
     @Bean
-    public Job firstJob(JobRepository jobRepository, Step firstStep, Step secondStep) {
-        return new JobBuilder("firstJob", jobRepository)
-                .incrementer(new RunIdIncrementer())
-                .start(firstStep)
-                .next(secondStep)
-                .listener(firstJobListener)
-                .build();
-    }
-
-    @Bean
-    public Step firstStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("firstStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("This is the first tasklet step");
-                    System.out.println("SEC = " + chunkContext.getStepContext().getStepExecutionContext());
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
-                .listener(firstStepListener)
-                .build();
-    }
-
-    @Bean
-    public Step secondStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("secondStep", jobRepository)
-                .tasklet(secondTasklet, transactionManager)
-                .build();
-    }
-
-    @Bean
-    public Job secondJob(JobRepository jobRepository, Step firstChunkStep, Step thirdStep) {
-        return new JobBuilder("secondJob", jobRepository)
+    public Job chunkJob(JobRepository jobRepository, Step firstChunkStep) {
+        return new JobBuilder("chunkJob", jobRepository)
                 .incrementer(new RunIdIncrementer())
                 .start(firstChunkStep)
-                .next(thirdStep)
                 .build();
     }
 
     @Bean
-    public Step firstChunkStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step firstChunkStep(JobRepository jobRepository, PlatformTransactionManager transactionManager,
+                               FlatFileItemReader<StudentCsv> studentCsvReader) {
         return new StepBuilder("firstChunkStep", jobRepository)
-                .<Integer, Long>chunk(3, transactionManager)
-                .reader(firstItemReader)
-                .processor(firstItemProcessor)
+                .<StudentCsv, StudentCsv>chunk(3, transactionManager)
+                .reader(studentCsvReader)
                 .writer(firstItemWriter)
                 .build();
     }
 
     @Bean
-    public Step thirdStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new StepBuilder("thirdStep", jobRepository)
-                .tasklet(secondTasklet, transactionManager)
-                .build();
+    @StepScope
+    public FlatFileItemReader<StudentCsv> studentCsvReader(
+            @Value("#{jobParameters['inputFile']}") FileSystemResource fileSystemResource
+    ) {
+        FlatFileItemReader<StudentCsv> flatFileItemReader =
+                new FlatFileItemReader<>();
+        flatFileItemReader.setResource(fileSystemResource);
+
+        DefaultLineMapper<StudentCsv> lineMapper =
+                new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer lineTokenizer =
+                new DelimitedLineTokenizer();
+        lineTokenizer.setNames("Id", "First Name", "Last Name", "Email");
+        lineTokenizer.setDelimiter("|");
+
+        BeanWrapperFieldSetMapper<StudentCsv> fieldSetMapper =
+                new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(StudentCsv.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer);
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+
+        flatFileItemReader.setLineMapper(lineMapper);
+        flatFileItemReader.setLinesToSkip(1);
+
+        return flatFileItemReader;
     }
 
 }
--- src/main/java/com/empresa/proyecto/writer/FirstItemWriter.java
+++ src/main/java/com/empresa/proyecto/writer/FirstItemWriter.java
@@ -1,14 +1,15 @@
 package com.empresa.proyecto.writer;
 
+import com.empresa.proyecto.model.StudentCsv;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FirstItemWriter implements ItemWriter<Long> {
+public class FirstItemWriter implements ItemWriter<StudentCsv> {
 
     @Override
-    public void write(Chunk<? extends Long> chunk) throws Exception {
+    public void write(Chunk<? extends StudentCsv> chunk) throws Exception {
         System.out.println("Inside Item Writer");
         chunk.forEach(System.out::println);
     }
